6aadb8d077f33e4a162887ffd8a18738a697dc94
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-23 13:02:58 +0200

    wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/main.css';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <p>something else</p>
+    <App />
   </React.StrictMode>,
 )
