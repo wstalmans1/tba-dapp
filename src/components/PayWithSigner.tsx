5920d187c5ddd891498e9db22f1572ac58c31b1a
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-25 08:29:09 +0200

    latest update

--- src/components/PayWithSigner.tsx
+++ src/components/PayWithSigner.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const PayWithSigner: React.FC = () => (
-  <div className="flex grow w-full h-full flex-col px-3 md:px-2">
-    <div className="flex grow flex-row justify-between space-x-2 bg-white bg-opacity-5 w-full rounded-lg md:flex-col md:space-x-0 md:space-y-2">
-      <h1>Pay with Signer</h1>
-      <p>This is the content for Paywithsigner.</p>
+  <div className="flex grow w-full h-full flex-col">
+    <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <h1>Pay with signer</h1>
+      <p>This is the content for PayWithSigner.</p>
     </div>
   </div>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,13 @@ import './styles/main.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
+import { mainnet, polygon, optimism, arbitrum, base, sepolia } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: 'YOUR_PROJECT_ID',
-    chains: [mainnet, polygon, optimism, arbitrum, base],
+    chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
     ssr: false, // If your dApp uses server side rendering (SSR)
 });
   
